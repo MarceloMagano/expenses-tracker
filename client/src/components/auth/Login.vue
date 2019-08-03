398a5afeeafc6c75053c93d041fee53aabea5f82
Marcelo Magano <marcelo.magano99@gmail.com>
2019-08-03 12:10:09 +0100

    working on frontend using vue

---
+++ client/src/components/auth/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class>
+    <h4>Login</h4>
+    <form>
+      <label form="email">Email Adress</label>
+      <div>
+        <input type="email" name="email" id="email" v-model="email" required autofocus />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <div>
+          <input id="password" type="password" v-model="password" required />
+        </div>
+      </div>
+      <div>
+        <button type="submit" @click="handleSubmit">Login</button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import { server } from '../../helper'
+import router from '../../router'
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleSubmit (e) {
+      e.preventDefault()
+      let user = {
+        email: this.email,
+        password: this.password
+      }
+      if (this.password.length > 0) {
+        axios.post(`${server.baseUrl}/auth/login`, user).then(data => {
+          router.push({ name: 'home' })
+        })
+        // post login
+        // set jwt to localStorage
+      }
+    }
+  }
+}
+</script>
---
+++ client/src/components/auth/Register.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class>
+    <h4>Register</h4>
+    <form>
+      <label form="name">Name</label>
+      <div>
+        <input type="name" name="name" id="name" v-model="name" required autofocus />
+      </div>
+
+      <label form="email">Email Adress</label>
+      <div>
+        <input type="email" name="email" id="email" v-model="email" required />
+      </div>
+
+      <label for="password">Password</label>
+      <div>
+        <input id="password" type="password" v-model="password" required />
+      </div>
+
+      <label for="password-confirm">Confirm Password</label>
+      <div>
+        <input id="password-confirm" type="password" v-model="password_confirmation" required />
+      </div>
+
+      <div>
+        <button type="submit" @click="handleSubmit">Register</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: ""
+    }
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+
+      if (
+        this.password === this.password_confirmation &&
+        this.password.length > 0
+      ) {
+        // post register
+        // redirect to login
+      } else {
+        this.password = "";
+        this.passwordConfirm = "";
+
+        return alert("Passwords do not match");
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
